0cfda6c6b5830b3bc3a0b757633a6bec41c08bc0
Kevin <kevin.haya@yahoo.com>
2022-08-29 23:08:29 +0800

    Finished positions

--- src/index.js
+++ src/index.js
@@ -1,1 +1,15 @@
-console.log("help")
+import './style.css';
+// import Icon from './icon.png';
+
+const content = document.getElementById("content")
+function component() {
+    const element = document.createElement("main");
+    element.innerHTML = `
+
+            <div>Chico</div>
+            <div>chicken & coffee</div>
+
+    `;
+    return element;
+}
+content.appendChild(component());
